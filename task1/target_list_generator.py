88fe592088bb8c3627cb26a5049a9d9bec71645d
m <autoacceleration42@gmail.com>
2026-02-14 03:20:02 +0300

    task1

---
+++ task1/target_list_generator.py
@@ -0,0 +1,16 @@
+def fill_links_to_file(filename: str) -> None:
+    """Generate links to crawl."""
+    # записываем ссылки в файл
+    with open(filename, "w") as f:
+        for i in range(108 + 1):
+            f.write(f"https://ilibrary.ru/text/1544/p.{i}/index.html\n")
+
+
+def main():
+    """Main function."""
+    fill_links_to_file("target_list.txt")
+    print("Complete!")
+
+
+if __name__ == "__main__":
+    main()
