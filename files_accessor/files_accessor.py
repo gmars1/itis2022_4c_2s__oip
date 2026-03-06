cbdada86c20324edca5f01ff5726250497aa0c8b
m <autoacceleration42@gmail.com>
2026-03-07 01:15:10 +0300

    refactor: task2

--- files_accessor/files_accessor.py
+++ files_accessor/files_accessor.py
@@ -2,6 +2,12 @@ TASK1_TARGET_LIST = "task1/target_list.txt"
 TASK1_CRAWLED = "task1/crawled"
 TASK1_INDEX = "task1/index.txt"
 
+TASK2_TOKENS = "task2/tokens.txt"
+TASK2_LEMMAS = "task2/lemmas.txt"
+
+TASK2_TOKENS_FOLDER = "task2/tokens/"
+TASK2_LEMMAS_FOLDER = "task2/lemmas/"
+
 
 class FilesFacade:
     def get_links(self, filename: str = TASK1_TARGET_LIST) -> list[str]:
