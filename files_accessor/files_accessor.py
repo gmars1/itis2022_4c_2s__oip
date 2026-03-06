d38bf7607f8b6d6f5609fdb24f8f30cb543ff66c
m <autoacceleration42@gmail.com>
2026-03-07 01:50:47 +0300

    refactor: task5

--- files_accessor/files_accessor.py
+++ files_accessor/files_accessor.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, Set
 
 TASK1_TARGET_LIST = "task1/target_list.txt"
@@ -24,7 +25,17 @@ class FilesFacade:
             links = [line for line in f]
         return links
 
-    # def get_crawled(self):
+    def get_text_from_docs(
+        self, doc_texts: Dict[int, str], foldername: str = TASK1_CRAWLED
+    ) -> None:
+        for root, _, files in os.walk(foldername):
+            for file in files:
+                filepath = os.path.join(root, file)
+                file_index = int(
+                    filepath.replace("task1/crawled/", "").replace(".txt", "")
+                )
+                with open(filepath, encoding="utf-8") as f:
+                    doc_texts.setdefault(file_index, f.read())
 
     # def get_index(self):
 
@@ -78,3 +89,37 @@ class FilesFacade:
                 lemma_tokens[lemma] = set(words)
                 for word in words:
                     token_to_lemma.setdefault(word, lemma)
+
+    def load_lemmas_file_to_set(self, all_lemmas: Set[str], file: str = TASK2_LEMMAS):
+        with open(file, encoding="utf-8") as f:
+            for line in f:
+                all_lemmas.add(line.strip().split()[0])
+
+    def load_tfidf_folders(
+        self,
+        tfidf_tokens: Dict[int, Dict[str, list[float]]],
+        tfidf_lemmas: Dict[int, Dict[str, list[float]]],
+        tfidf_tokens_folder: str = TASK4_TFIDF_TOKENS,
+        tfidf_lemmas_folder: str = TASK4_TFIDF_LEMMAS,
+    ):
+        for root, _, files in os.walk(tfidf_tokens_folder):
+            for file in files:
+                filepath = os.path.join(root, file)
+                with open(filepath, encoding="utf-8") as f:
+                    index = int(filepath[19:-4])
+                    data: Dict[str, list[float]] = dict()
+                    for line in f:
+                        splitted = line.split()
+                        data.setdefault(splitted[0], list(map(float, splitted[1:3])))
+                    tfidf_tokens.setdefault(index, data)
+
+        for root, _, files in os.walk(tfidf_lemmas_folder):
+            for file in files:
+                filepath = os.path.join(root, file)
+                with open(filepath, encoding="utf-8") as f:
+                    index = int(filepath[19:-4])
+                    data = dict()
+                    for line in f:
+                        splitted = line.split()
+                        data.setdefault(splitted[0], list(map(float, splitted[1:3])))
+                    tfidf_lemmas.setdefault(index, data)
